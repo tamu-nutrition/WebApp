bd0a8b9a15c5124a98dbe1ca8fdeca3fcb7a34be
root <root@localhost>
2021-10-03 01:24:57 +0000

    simplecov added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'simplecov'
+SimpleCov.start 
+# (you can try SimpleCov.start 'rails' instead to see if the coverage report is different, but we only require the one above)
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
