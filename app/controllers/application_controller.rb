ea58f8c6634b9a9548091600b2a68ea71817462d
root <root@localhost>
2021-10-24 01:40:27 +0000

    fixed detection of if user is logged in or not; added navbar to students page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+  
+  helper_method :current_admin
+
+  private
+
+  def current_admin
+    if session[:email]
+      return session[:email]
+    else
+      return nil
+    end
+  end
 end
