2d236ac92bfbe7baa75ae15c1302202f9ac8b33c
root <root@localhost>
2021-10-26 04:34:15 +0000

    deleted unnecessary files, changed database.yml to hopefully allow for actions output

--- app/models/other_event.rb
+++ app/models/other_event.rb
@@ -1,5 +1,4 @@
 class OtherEvent < ApplicationRecord
-
     validates :event_id, presence: true, uniqueness: true
     validates :point_worth, presence: true
     validates :event_type, presence: true
