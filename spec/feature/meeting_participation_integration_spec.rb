cbeb661386d73f75b8d36ce9ab4d1a452af5c64b
root <root@localhost>
2021-10-26 04:23:37 +0000

    added remove integration test for meeting participation

--- spec/feature/meeting_participation_integration_spec.rb
+++ spec/feature/meeting_participation_integration_spec.rb
@@ -8,8 +8,26 @@ def login
   click_link 'Sign in with Google'
 end
 
-RSpec.describe 'Adding/filtering a meeting participation', type: :feature do
-  scenario 'success inputs' do
+RSpec.describe 'Meeting Participation', type: :feature do
+
+  scenario 'deleting meeting participation' do
+    login()
+    visit meeting_participations_url
+    click_on 'New Meeting Participation'
+
+    fill_in 'meeting_participation_meeting_id', with: 1
+    fill_in 'meeting_participation_UIN', with: '12345'
+    click_on 'Create Meeting participation'
+    click_on 'Back'
+    #puts page.html
+    expect(page).to have_content('12345')
+    click_on 'Destroy'
+    expect(page).not_to have_content('12345')
+    Rails.application.env_config["omniauth.auth"] = nil
+    OmniAuth.config.mock_auth[:google] = nil
+  end
+
+  scenario 'adding/filtering meeting participation' do
     login()
     visit meeting_participations_url
     click_on 'New Meeting Participation'
@@ -25,4 +43,5 @@ RSpec.describe 'Adding/filtering a meeting participation', type: :feature do
     Rails.application.env_config["omniauth.auth"] = nil
     OmniAuth.config.mock_auth[:google] = nil
   end
+
 end
