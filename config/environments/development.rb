4f74b6762fc5c5cbcbf56ac14ac4aa553b47fea5
Kylan Lewis <45674401+kylanalewis@users.noreply.github.com>
2021-11-08 21:41:49 -0600

    Revert "Feature request2.0"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,14 @@
 require "active_support/core_ext/integer/time"
 
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '262230267345-euaqg4mejp3qn1cmrdtuhgpkssv0d9dk.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '262230267345-euaqg4mejp3qn1cmrdtuhgpkssv0d9dk.apps.googleusercontent.com'
-  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
--- db/migrate/20211104131831_add_type_to_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToRequests < ActiveRecord::Migration[6.1]
-  def change
-    add_column :requests, :request_type, :string
-  end
-end
