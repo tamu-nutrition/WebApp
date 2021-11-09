6614b6409e0c4ce9fa4b09cfb99405ed02d172a6
wkirby0310 <wkirby0310@gmail.com>
2021-11-09 17:54:14 -0600

    Update other_event.rb

--- app/models/other_event.rb
+++ app/models/other_event.rb
@@ -8,4 +8,5 @@ class OtherEvent < ApplicationRecord
   validates :point_worth, presence: true
   validates :event_type, presence: true
   validates :number_participation, presence: true
+  validates :name, presence: true
 end
