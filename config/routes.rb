f27ca1e9bffda0551ae5fb41e6e334646afa6b18
root <root@localhost>
2021-11-17 01:00:51 +0000

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'students#index'
+  root to: 'meeting_participations#new'
 end
