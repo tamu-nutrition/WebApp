050e9d2c199b082dd65f1a021d6b583cc0648e6f
root <root@localhost>
2021-11-22 04:51:11 +0000

    meeting participation increments meeting points; user is prompted to add their information if they are not already in the system; UIN autofilled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_050001) do
+ActiveRecord::Schema.define(version: 2021_11_22_023847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_050001) do
 
   create_table "meeting_participations", force: :cascade do |t|
     t.integer "meeting_id"
-    t.string "UIN"
+    t.integer "UIN"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -137,5 +137,6 @@ ActiveRecord::Schema.define(version: 2021_11_16_050001) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "meeting_participations", "students", column: "UIN", primary_key: "UIN"
   add_foreign_key "requests", "students", column: "UIN", primary_key: "UIN"
 end
